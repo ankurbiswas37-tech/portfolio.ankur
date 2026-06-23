6ff5276d8e1221593a87373ee2ad45311eabb1bd
Infinix Online <mac@MACs-MacBook-Pro.local>
2026-06-23 07:18:25 +0600

    fixed contact schema import name mismatch in index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,4 +1,4 @@
-import { contact } from './contact';
+import { contactSchema } from './contact'; // 👈 সঠিক নাম এবং কার্লি ব্র্যাকেট সহ
 import { portfolioProject } from './project';
 
-export const schemaTypes = [contact, portfolioProject];
+export const schemaTypes = [contactSchema, portfolioProject];
